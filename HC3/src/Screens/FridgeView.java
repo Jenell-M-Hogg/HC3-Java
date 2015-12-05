554963f789feb7045e459d7f990743d53054c3fe
Jenell-M-Hogg <jenellmhogg@live.com>
2015-12-05 17:01:53 -0500

    Try again

---
+++ HC3/src/Screens/FridgeView.java
@@ -0,0 +1,18 @@
+package Screens;
+
+import javax.swing.JPanel;
+
+import Global.Constants;
+
+public class FridgeView extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public FridgeView() {
+		this.setSize(Constants.FRAME_WIDTH, Constants.FRAME_HEIGHT);
+		
+
+	}
+
+}
