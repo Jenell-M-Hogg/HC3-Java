4bd177791a25bb3f7fc5f9191feeb8add9898db2
hereandnowlive <mitchell_27@live.ca>
2015-12-06 18:05:36 -0500

    started working on main menu

--- HC3/src/Screens/MainMenu.java
+++ HC3/src/Screens/MainMenu.java
@@ -1,14 +1,32 @@
 package Screens;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
 
 public class MainMenu extends JPanel {
+	private final Action action = new SwingAction();
 
 	/**
 	 * Create the panel.
 	 */
 	public MainMenu() {
+		
+		JComboBox comboBox = new JComboBox();
+		add(comboBox);
 
 	}
 
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
 }
