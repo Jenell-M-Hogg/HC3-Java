201b0c9876ad66e58745ba5c33c16d6dadcf9ab6
Jenell-M-Hogg <jenellmhogg@live.com>
2015-12-07 16:37:14 -0500

    Updates To Bars

--- HC3/src/Widgets/BottomBar.java
+++ HC3/src/Widgets/BottomBar.java
@@ -2,50 +2,12 @@ package Widgets;
 
 import javax.swing.JPanel;
 
-import Global.Constants;
-import javax.swing.JButton;
-import java.awt.GridLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-
-public class BottomBar extends JPanel {
+public class BottomBar extends HeaderBar {
 
 	/**
 	 * Create the panel.
 	 */
 	public BottomBar() {
-		this.setSize(Constants.FRAME_WIDTH, Constants.FRAME_HEIGHT/15);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{133, 133, 133, 0};
-		gridBagLayout.rowHeights = new int[]{40, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		setLayout(gridBagLayout);
-		
-		JButton addButton = new JButton("New button");
-		GridBagConstraints gbc_addButton = new GridBagConstraints();
-		gbc_addButton.fill = GridBagConstraints.BOTH;
-		gbc_addButton.insets = new Insets(0, 0, 0, 5);
-		gbc_addButton.gridx = 0;
-		gbc_addButton.gridy = 0;
-		add(addButton, gbc_addButton);
-		
-		JButton removeMoveButton = new JButton("New button");
-		GridBagConstraints gbc_removeMoveButton = new GridBagConstraints();
-		gbc_removeMoveButton.fill = GridBagConstraints.BOTH;
-		gbc_removeMoveButton.insets = new Insets(0, 0, 0, 5);
-		gbc_removeMoveButton.gridx = 1;
-		gbc_removeMoveButton.gridy = 0;
-		add(removeMoveButton, gbc_removeMoveButton);
-		
-		JButton editFridgeDetails = new JButton("New button");
-		GridBagConstraints gbc_editFridgeDetails = new GridBagConstraints();
-		gbc_editFridgeDetails.fill = GridBagConstraints.BOTH;
-		gbc_editFridgeDetails.gridx = 2;
-		gbc_editFridgeDetails.gridy = 0;
-		add(editFridgeDetails, gbc_editFridgeDetails);
-		
 		
 
 	}
--- HC3/src/Widgets/HeaderBar.java
+++ HC3/src/Widgets/HeaderBar.java
@@ -2,13 +2,108 @@ package Widgets;
 
 import javax.swing.JPanel;
 
-public class HeaderBar extends JPanel {
+import Global.Constants;
 
+import javax.swing.JButton;
+
+import java.awt.Button;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+
+public class HeaderBar extends JPanel {
+	JButton rightButton;
+	JButton middleButton;
+	JButton leftButton;
 	/**
 	 * Create the panel.
 	 */
 	public HeaderBar() {
+		this.setSize((int) (Constants.FRAME_WIDTH), Constants.FRAME_HEIGHT/15);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{133, 133, 133, 0};
+		gridBagLayout.rowHeights = new int[]{40, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		rightButton = new JButton("Add An Item");
+		rightButton.setIcon(new ImageIcon(HeaderBar.class.getResource("/images/images/addIcon.png")));
+		GridBagConstraints gbc_addButton = new GridBagConstraints();
+		gbc_addButton.fill = GridBagConstraints.BOTH;
+		gbc_addButton.insets = new Insets(0, 0, 0, 5);
+		gbc_addButton.gridx = 0;
+		gbc_addButton.gridy = 0;
+		
+		
+		//Add action listener
+		rightButton.addActionListener(new ActionListener(){
 
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				addNewItem();
+				
+			}
+
+
+			
+		});
+		
+		
+		add(rightButton, gbc_addButton);
+		
+		middleButton = new JButton("Move/Remove Items");
+		GridBagConstraints gbc_removeMoveButton = new GridBagConstraints();
+		gbc_removeMoveButton.fill = GridBagConstraints.BOTH;
+		gbc_removeMoveButton.insets = new Insets(0, 0, 0, 5);
+		gbc_removeMoveButton.gridx = 1;
+		gbc_removeMoveButton.gridy = 0;
+		
+		middleButton.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//TODO
+				
+			}
+
+
+			
+		});
+		add(middleButton, gbc_removeMoveButton);
+		
+		leftButton = new JButton("Edit Fridge Details");
+		GridBagConstraints gbc_editFridgeDetails = new GridBagConstraints();
+		gbc_editFridgeDetails.fill = GridBagConstraints.BOTH;
+		gbc_editFridgeDetails.gridx = 2;
+		gbc_editFridgeDetails.gridy = 0;
+		
+		leftButton.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//TODO
+				
+			}
+
+
+			
+		});
+		add(leftButton, gbc_editFridgeDetails);
+		
+		
+
+	}
+	
+	
+	private void addNewItem() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
