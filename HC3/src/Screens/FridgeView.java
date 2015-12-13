c10b159bd61edd6d739bfce1518ef818fe6b2abd
Jenell-M-Hogg <jenellmhogg@live.com>
2015-12-13 18:28:14 -0500

    FridgeView

--- HC3/src/Screens/FridgeView.java
+++ HC3/src/Screens/FridgeView.java
@@ -4,10 +4,13 @@ import javax.swing.JPanel;
 
 import Global.Constants;
 import Repository.Fridge;
+import Repository.Item;
 import Widgets.HeaderBar;
 import Widgets.BottomBar;
+import Widgets.ItemPanel;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
@@ -18,17 +21,32 @@ import javax.swing.JTabbedPane;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.DefaultComboBoxModel;
+
 import Global.FridgeLocation;
+
 import java.awt.FlowLayout;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import Widgets.Compartment;
+import java.awt.GridLayout;
 
 public class FridgeView extends JPanel {
+	private Fridge fridge;
+	
 	private JLabel fridgeLabel;
 	private JLabel messageLbl;
+	private JPanel fridgePanel;
 
 	/**
 	 * Create the panel.
+	 * @throws URISyntaxException 
+	 * @throws IOException 
 	 */
-	public FridgeView(Fridge fridge) {
+	public FridgeView(Fridge fridge) throws IOException, URISyntaxException {
+		this.fridge=fridge;
+		
 		this.setSize(Constants.FRAME_WIDTH, Constants.FRAME_HEIGHT);
 		setLayout(new BorderLayout(0, 0));
 		
@@ -70,29 +88,66 @@ public class FridgeView extends JPanel {
 		
 		JPanel panel_1 = new JPanel();
 		add(panel_1, BorderLayout.CENTER);
-		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[] {0, 0};
-		gbl_panel_1.columnWeights = new double[]{0.0, 1.0};
-		gbl_panel_1.rowWeights = new double[]{1.0};
-		panel_1.setLayout(gbl_panel_1);
+		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		
+		
+		
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Hello", "Yooo"}));
-		GridBagConstraints gbc_comboBox = new GridBagConstraints();
-		gbc_comboBox.anchor = GridBagConstraints.NORTHWEST;
-		gbc_comboBox.weightx = 0.25;
-		gbc_comboBox.insets = new Insets(1, 1, 0, 5);
-		gbc_comboBox.gridx = 0;
-		gbc_comboBox.gridy = 0;
-		panel_1.add(comboBox, gbc_comboBox);
+		panel_1.add(comboBox);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
-		gbc_comboBox_1.anchor = GridBagConstraints.NORTHEAST;
-		gbc_comboBox_1.gridx = 1;
-		gbc_comboBox_1.gridy = 0;
-		panel_1.add(comboBox_1, gbc_comboBox_1);
+		fridgePanel = new JPanel();
+		fridgePanel.setBackground(Color.WHITE);
+		panel_1.add(fridgePanel);
+		GridBagLayout gbl_fridgePanel = new GridBagLayout();
+		gbl_fridgePanel.columnWidths = new int[] {150};
+		gbl_fridgePanel.rowHeights = new int[] {150};
+		gbl_fridgePanel.columnWeights = new double[]{0.0};
+		gbl_fridgePanel.rowWeights = new double[]{0.0};
+		fridgePanel.setLayout(gbl_fridgePanel);
+		
+		
+		
+		initializeCompartments();
+		fillCompartments();
+	}
 
+	private void initializeCompartments() throws IOException, URISyntaxException {
+		// TODO Auto-generated method stub
+		Compartment compartment = new Compartment(false);
+		GridBagConstraints gbc_compartment = new GridBagConstraints();
+		gbc_compartment.fill = GridBagConstraints.BOTH;
+		gbc_compartment.anchor = GridBagConstraints.NORTHWEST;
+		gbc_compartment.gridx = 0;
+		gbc_compartment.gridy = 0;
+		fridgePanel.add(compartment, gbc_compartment);
+		
+		
+		ArrayList<Item> items= fridge.getItems();
+		ArrayList<ItemPanel> panels= new ArrayList<ItemPanel>();
+		
+		for(Item item: items){
+			panels.add(new ItemPanel(item));
+		}
+		
+		
+		for(ItemPanel panel : panels){
+			compartment.add(panel);
+		}
+		
+		
+	}
+
+	private void fillCompartments() throws IOException, URISyntaxException {
+		// TODO Auto-generated method stub
+	
+		
+		
+		
+		
+		
 	}
 
 }
