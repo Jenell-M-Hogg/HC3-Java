82f6111d4c3eb42c3d29982480f9028c1d5b637c
hereandnowlive <mitchell_27@live.ca>
2015-12-14 12:54:58 -0500

    first fridge view parts setup

--- HC3/src/Screens/FridgeView.java
+++ HC3/src/Screens/FridgeView.java
@@ -12,6 +12,7 @@ import Widgets.ItemPanel;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
@@ -33,6 +34,8 @@ import java.util.ArrayList;
 import Widgets.Compartment;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FridgeView extends JPanel {
 	private Fridge fridge;
@@ -105,17 +108,19 @@ public class FridgeView extends JPanel {
 		fridgePanel.setBackground(Color.WHITE);
 		panel_1.add(fridgePanel, BorderLayout.CENTER);
 		GridBagLayout gbl_fridgePanel = new GridBagLayout();
-		gbl_fridgePanel.columnWidths = new int[] {0};
-		gbl_fridgePanel.rowHeights = new int[] {0, 0};
-		gbl_fridgePanel.columnWeights = new double[]{1.0};
-		gbl_fridgePanel.rowWeights = new double[]{0.0, 1.0};
+		gbl_fridgePanel.columnWidths = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_fridgePanel.rowHeights = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_fridgePanel.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+		gbl_fridgePanel.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0};
 		fridgePanel.setLayout(gbl_fridgePanel);
 		
 		compartment = new Compartment();
 		GridBagConstraints gbc_compartment = new GridBagConstraints();
+		gbc_compartment.gridheight = 19;
+		gbc_compartment.gridwidth = 13;
 		gbc_compartment.ipady = 1;
 		gbc_compartment.ipadx = 1;
-		gbc_compartment.insets = new Insets(5, 5, 5, 0);
+		gbc_compartment.insets = new Insets(2, 2, 5, 2);
 		gbc_compartment.weighty = 1.0;
 		gbc_compartment.weightx = 1.0;
 		gbc_compartment.fill = GridBagConstraints.BOTH;
@@ -124,36 +129,207 @@ public class FridgeView extends JPanel {
 		gbc_compartment.gridy = 0;
 		fridgePanel.add(compartment, gbc_compartment);
 		
-		JPanel panel_2 = new JPanel();
-		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
-		gbc_panel_2.fill = GridBagConstraints.BOTH;
-		gbc_panel_2.gridx = 0;
-		gbc_panel_2.gridy = 1;
-		fridgePanel.add(panel_2, gbc_panel_2);
 		
 		
+		
+		initializeCompartments(comboBox_2);
+		
+		comboBox_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					initializeCompartments(comboBox_2);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (URISyntaxException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
 		
 	}
 
-	public void initializeCompartments() throws IOException, URISyntaxException {
+	
+	
+	
+	public void initializeCompartments(JComboBox comboBox) throws IOException, URISyntaxException {
 		// TODO Auto-generated method stub
-
-		
-		fridgePanel.validate();
-		
-		
-		ArrayList<Item> items= fridge.getItems();
-		ArrayList<ItemPanel> panels= new ArrayList<ItemPanel>();
-		
-		for(Item item: items){
-			panels.add(new ItemPanel(item));
+		Component[] comp = fridgePanel.getComponents();
+		for(Component k : comp){
+			fridgePanel.remove(k);
 		}
 		
 		
-		compartment.AddItems(panels);
+		if(comboBox.getModel().getSelectedItem().equals("Fridge Body")){
+		
+		
+			compartment = new Compartment();
+			GridBagConstraints gbc_compartment = new GridBagConstraints();
+			gbc_compartment.gridheight = 5;
+			gbc_compartment.gridwidth = 13;
+			gbc_compartment.ipady = 1;
+			gbc_compartment.ipadx = 1;
+			gbc_compartment.insets = new Insets(2, 2, 5, 2);
+			gbc_compartment.weighty = 1.0;
+			gbc_compartment.weightx = 1.0;
+			gbc_compartment.fill = GridBagConstraints.BOTH;
+			gbc_compartment.anchor = GridBagConstraints.NORTHWEST;
+			gbc_compartment.gridx = 0;
+			gbc_compartment.gridy = 0;
+			fridgePanel.add(compartment, gbc_compartment);
+			
+			Compartment compartment_1 = new Compartment();
+			GridBagConstraints gbc_compartment_1 = new GridBagConstraints();
+			gbc_compartment_1.gridheight = 5;
+			gbc_compartment_1.gridwidth = 13;
+			gbc_compartment_1.insets = new Insets(2, 2, 5, 2);
+			gbc_compartment_1.fill = GridBagConstraints.BOTH;
+			gbc_compartment_1.gridx = 0;
+			gbc_compartment_1.gridy = 5;
+			fridgePanel.add(compartment_1, gbc_compartment_1);
+			
+			Compartment compartment_2 = new Compartment();
+			GridBagConstraints gbc_compartment_2 = new GridBagConstraints();
+			gbc_compartment_2.gridheight = 5;
+			gbc_compartment_2.gridwidth = 13;
+			gbc_compartment_2.insets = new Insets(2, 2, 5, 2);
+			gbc_compartment_2.fill = GridBagConstraints.BOTH;
+			gbc_compartment_2.gridx = 0;
+			gbc_compartment_2.gridy = 10;
+			fridgePanel.add(compartment_2, gbc_compartment_2);
+			
+			Compartment compartment_3 = new Compartment();
+			GridBagConstraints gbc_compartment_3 = new GridBagConstraints();
+			gbc_compartment_3.gridwidth = 6;
+			gbc_compartment_3.gridheight = 4;
+			gbc_compartment_3.insets = new Insets(2, 2, 2, 5);
+			gbc_compartment_3.fill = GridBagConstraints.BOTH;
+			gbc_compartment_3.gridx = 0;
+			gbc_compartment_3.gridy = 15;
+			fridgePanel.add(compartment_3, gbc_compartment_3);
+			
+			Compartment compartment_4 = new Compartment();
+			GridBagConstraints gbc_compartment_4 = new GridBagConstraints();
+			gbc_compartment_4.gridheight = 4;
+			gbc_compartment_4.gridwidth = 7;
+			gbc_compartment_4.insets = new Insets(2, 2, 2, 2);
+			gbc_compartment_4.fill = GridBagConstraints.BOTH;
+			gbc_compartment_4.gridx = 6;
+			gbc_compartment_4.gridy = 15;
+			fridgePanel.add(compartment_4, gbc_compartment_4);
+			
+			ArrayList<Item> items= fridge.getItems();
+			ArrayList<ItemPanel> panels= new ArrayList<ItemPanel>();
+			
+			for(Item item: items){
+				panels.add(new ItemPanel(item));
+			}
+			
+			
+			compartment.AddItems(panels);
+			
+		}
+		
+		
+		
+		if(comboBox.getModel().getSelectedItem().equals("Door")){
+			
+			compartment = new Compartment();
+			GridBagConstraints gbc_compartment = new GridBagConstraints();
+			gbc_compartment.gridheight = 7;
+			gbc_compartment.gridwidth = 13;
+			gbc_compartment.ipady = 1;
+			gbc_compartment.ipadx = 1;
+			gbc_compartment.insets = new Insets(2, 2, 5, 2);
+			gbc_compartment.weighty = 1.0;
+			gbc_compartment.weightx = 1.0;
+			gbc_compartment.fill = GridBagConstraints.BOTH;
+			gbc_compartment.anchor = GridBagConstraints.NORTHWEST;
+			gbc_compartment.gridx = 0;
+			gbc_compartment.gridy = 0;
+			fridgePanel.add(compartment, gbc_compartment);
+			
+			Compartment compartment_1 = new Compartment();
+			GridBagConstraints gbc_compartment_1 = new GridBagConstraints();
+			gbc_compartment_1.gridheight = 6;
+			gbc_compartment_1.gridwidth = 13;
+			gbc_compartment_1.insets = new Insets(0, 0, 5, 0);
+			gbc_compartment_1.fill = GridBagConstraints.BOTH;
+			gbc_compartment_1.gridx = 0;
+			gbc_compartment_1.gridy = 7;
+			fridgePanel.add(compartment_1, gbc_compartment_1);
+			
+			Compartment compartment_2 = new Compartment();
+			GridBagConstraints gbc_compartment_2 = new GridBagConstraints();
+			gbc_compartment_2.gridheight = 6;
+			gbc_compartment_2.gridwidth = 13;
+			gbc_compartment_2.insets = new Insets(0, 0, 5, 5);
+			gbc_compartment_2.fill = GridBagConstraints.BOTH;
+			gbc_compartment_2.gridx = 0;
+			gbc_compartment_2.gridy = 13;
+			fridgePanel.add(compartment_2, gbc_compartment_2);
+			
+		}
+		
+		if(comboBox.getModel().getSelectedItem().equals("Freezer")){
+			
+			compartment = new Compartment();
+			GridBagConstraints gbc_compartment = new GridBagConstraints();
+			gbc_compartment.gridheight = 10;
+			gbc_compartment.gridwidth = 13;
+			gbc_compartment.ipady = 1;
+			gbc_compartment.ipadx = 1;
+			gbc_compartment.insets = new Insets(2, 2, 5, 2);
+			gbc_compartment.weighty = 1.0;
+			gbc_compartment.weightx = 1.0;
+			gbc_compartment.fill = GridBagConstraints.BOTH;
+			gbc_compartment.anchor = GridBagConstraints.NORTHWEST;
+			gbc_compartment.gridx = 0;
+			gbc_compartment.gridy = 0;
+			fridgePanel.add(compartment, gbc_compartment);
+			
+			Compartment compartment_1 = new Compartment();
+			GridBagConstraints gbc_compartment_1 = new GridBagConstraints();
+			gbc_compartment_1.gridheight = 9;
+			gbc_compartment_1.gridwidth = 13;
+			gbc_compartment_1.insets = new Insets(0, 0, 5, 0);
+			gbc_compartment_1.fill = GridBagConstraints.BOTH;
+			gbc_compartment_1.gridx = 0;
+			gbc_compartment_1.gridy = 10;
+			fridgePanel.add(compartment_1, gbc_compartment_1);
+			
+		}
+		
+		if(comboBox.getModel().getSelectedItem().equals("Freezer Door")){
+			compartment = new Compartment();
+			GridBagConstraints gbc_compartment = new GridBagConstraints();
+			gbc_compartment.gridheight = 19;
+			gbc_compartment.gridwidth = 13;
+			gbc_compartment.ipady = 1;
+			gbc_compartment.ipadx = 1;
+			gbc_compartment.insets = new Insets(2, 2, 5, 2);
+			gbc_compartment.weighty = 1.0;
+			gbc_compartment.weightx = 1.0;
+			gbc_compartment.fill = GridBagConstraints.BOTH;
+			gbc_compartment.anchor = GridBagConstraints.NORTHWEST;
+			gbc_compartment.gridx = 0;
+			gbc_compartment.gridy = 0;
+			fridgePanel.add(compartment, gbc_compartment);
+			
+		}
+		
+		
+		
+		fridgePanel.revalidate();
+		fridgePanel.repaint();
 		
 	}
 
+	
+	
+	
+	
 	public Fridge getFridge() {
 		return fridge;
 	}
