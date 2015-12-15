caab8589aa36d1ac36403eafa9b83891fe922887
Jenell-M-Hogg <jenellmhogg@live.com>
2015-12-14 19:01:51 -0500

    Buttons change now

--- HC3/src/Widgets/HeaderBar.java
+++ HC3/src/Widgets/HeaderBar.java
@@ -24,6 +24,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.Button;
+import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
@@ -49,6 +50,8 @@ public class HeaderBar extends JPanel {
 	JButton rightButton;
 	JButton middleButton;
 	JButton leftButton;
+	
+	boolean inRemoveMode=false;
 	//CategoryList categoryList = CategoryList.getInstance();
 	
 	/**
@@ -65,164 +68,225 @@ public class HeaderBar extends JPanel {
 		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
+		
+		boolean isRemoveMode=false;
+		setButtons(false);
+		
+		
+		
+	}
 
-		rightButton = new JButton("Add An Item");
-		rightButton.setIcon(new ImageIcon(HeaderBar.class.getResource("/images/images/addIcon.png")));
+	private void setButtons(boolean b) {
+		
+		ActionListener r= new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(inRemoveMode){
+					moveItems();
+				}
+				else{
+					try {
+						addNewItem();
+					} catch (IOException | URISyntaxException | ParseException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+			
+		};
+		ActionListener m= new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(inRemoveMode){
+					removeItems();
+				}
+				else{
+					changeMode();
+				}
+			}
+			
+		};
+		ActionListener l= new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(inRemoveMode){
+					changeMode();
+				}
+				else{
+					methodName();
+				}
+			}
+			
+		};
+		
+		
+		if(!b){
+
+			setButton(0, "Add an Item", true, "/images/images/addIcon.png", r);
+			setButton(1, "Move/Remove Items", false, "", m);
+			setButton(2, "Edit Fridge Details", false, "", l);
+			
+		}
+		else{
+			setButton(0, "Move Items", false, "", r);
+			setButton(1, "Remove Items", false, "", m);
+			setButton(2, "Cancel", false, "", l);
+		}
+
+		
+	}
+
+	protected void moveItems() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void removeItems() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setButton(int i, String label, boolean hasImage, String imagePath, ActionListener l) {
+		// TODO Auto-generated method stub
+		
+		rightButton = new JButton(label);
+		if(hasImage){
+			rightButton.setIcon(new ImageIcon(HeaderBar.class.getResource(imagePath)));
+		}
+		
 		GridBagConstraints gbc_addButton = new GridBagConstraints();
 		gbc_addButton.fill = GridBagConstraints.BOTH;
 		gbc_addButton.insets = new Insets(0, 0, 0, 5);
-		gbc_addButton.gridx = 0;
+		gbc_addButton.gridx = i;
 		gbc_addButton.gridy = 0;
 
 
 		//Add action listener
-		rightButton.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-					addNewItem();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (URISyntaxException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (ParseException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-
-			}
-
-
-
-		});
-
-
+		rightButton.addActionListener(l);
+		
 		add(rightButton, gbc_addButton);
-
-		middleButton = new JButton("Move/Remove Items");
-		GridBagConstraints gbc_removeMoveButton = new GridBagConstraints();
-		gbc_removeMoveButton.fill = GridBagConstraints.BOTH;
-		gbc_removeMoveButton.insets = new Insets(0, 0, 0, 5);
-		gbc_removeMoveButton.gridx = 1;
-		gbc_removeMoveButton.gridy = 0;
-
-		middleButton.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				//TODO
-
-			}
-
-
-
-		});
-		add(middleButton, gbc_removeMoveButton);
-
-		leftButton = new JButton("Edit Fridge Details");
-		GridBagConstraints gbc_editFridgeDetails = new GridBagConstraints();
-		gbc_editFridgeDetails.fill = GridBagConstraints.BOTH;
-		gbc_editFridgeDetails.gridx = 2;
-		gbc_editFridgeDetails.gridy = 0;
-
-		leftButton.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-				ShoppingListView shoppingview = null;
-				ListView listview = null;
-
-				if(getParent() instanceof ListView){
-					listview = (ListView)getParent();
-				}else if(getParent() instanceof ShoppingListView){
-					shoppingview = (ShoppingListView)getParent();
-				}
-
-				Object[] options = {"Confirm Name",
-				"Remove Fridge"};
-
-				Object[] optionsshop = {"Confirm Name",
-				"Remove Shopping List"};
-
-				JTextField Field = new JTextField(5);
-
-				JPanel myPanel = new JPanel();
-				myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-				myPanel.add(new JLabel("Enter New Name"));
-				myPanel.add(Field);
-
-
-
-				if(listview instanceof ListView){
-					int answer = JOptionPane.showOptionDialog(null,
-							myPanel,
-							"Fridge Details",
-							JOptionPane.YES_NO_OPTION,
-							JOptionPane.QUESTION_MESSAGE,
-							null,     //do not use a custom Icon
-							options,  //the titles of buttons
-							options[0]);
-
-					if(answer == JOptionPane.YES_OPTION){
-						MainMenu.mainmenuInstance.UpdateNameLabels(listview, null, Field.getText(),listview.getFridge().returnName());
-						listview.getFridge().setNewName(Field.getText());
-						listview.revalidate();
-						listview.repaint();
-					}else if(answer == JOptionPane.NO_OPTION){
-						Object[] optionsalt2 = {"Please continue",
-						"Cancel"};
-						int result = JOptionPane.showOptionDialog(null,
-								"This action will permanently remove selected objects. Are you sure you want to proceed?",
-								"Confirmation PopUp",
-								JOptionPane.YES_NO_OPTION,
-								JOptionPane.WARNING_MESSAGE,
-								null,     //do not use a custom Icon
-								optionsalt2,  //the titles of buttons
-								optionsalt2[0]); //default button title
-						if(result == JOptionPane.YES_OPTION)
-							MainMenu.mainmenuInstance.DestroyObject(listview, null,listview.getFridge().returnName());
-					}
-				}else if(shoppingview instanceof ShoppingListView){
-					int answer = JOptionPane.showOptionDialog(null,
-							myPanel,
-							"Fridge Details",
-							JOptionPane.YES_NO_OPTION,
-							JOptionPane.QUESTION_MESSAGE,
-							null,     //do not use a custom Icon
-							optionsshop,  //the titles of buttons
-							optionsshop[0]);
-
-					if(answer == JOptionPane.YES_OPTION){
-						MainMenu.mainmenuInstance.UpdateNameLabels(null, shoppingview, Field.getText(),shoppingview.getShopList().getName());
-						shoppingview.getShopList().setName(Field.getText());
-						shoppingview.revalidate();
-						shoppingview.repaint();
-					}else if(answer == JOptionPane.NO_OPTION){
-						Object[] optionsalt = {"Please continue",
-						"Cancel"};
-						int result = JOptionPane.showOptionDialog(null,
-								"This action will permanently remove selected objects. Are you sure you want to proceed?",
-								"Confirmation PopUp",
-								JOptionPane.YES_NO_OPTION,
-								JOptionPane.WARNING_MESSAGE,
-								null,     //do not use a custom Icon
-								optionsalt,  //the titles of buttons
-								optionsalt[0]); //default button title
-						if(result == JOptionPane.YES_OPTION)
-							MainMenu.mainmenuInstance.DestroyObject(null, shoppingview,shoppingview.getShopList().getName());
-					}
-				}
-			}
-		});
-		add(leftButton, gbc_editFridgeDetails);
+		
 	}
+
+	protected void changeMode() {
+		this.inRemoveMode=!this.inRemoveMode;
+		
+		
+		Component[] comp=this.getComponents();
+		
+		for(Component b: comp){
+			if(b instanceof JButton){
+				this.remove(b);
+			}
+		}
+		
+		this.setButtons(inRemoveMode);
+		
+		this.validate();
+		this.repaint();
+		
+	}
+
 
 	private void addNewItem() throws IOException, URISyntaxException, ParseException {
 		JPanel addItemPopup = new AddItemPopup();
 		add(addItemPopup);
 	}
-}
+	
+	public void methodName(){
+	
+
+			ShoppingListView shoppingview = null;
+			ListView listview = null;
+
+			if(getParent() instanceof ListView){
+				listview = (ListView)getParent();
+			}else if(getParent() instanceof ShoppingListView){
+				shoppingview = (ShoppingListView)getParent();
+			}
+
+			Object[] options = {"Confirm Name",
+			"Remove Fridge"};
+
+			Object[] optionsshop = {"Confirm Name",
+			"Remove Shopping List"};
+
+			JTextField Field = new JTextField(5);
+
+			JPanel myPanel = new JPanel();
+			myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+			myPanel.add(new JLabel("Enter New Name"));
+			myPanel.add(Field);
+
+
+
+			if(listview instanceof ListView){
+				int answer = JOptionPane.showOptionDialog(null,
+						myPanel,
+						"Fridge Details",
+						JOptionPane.YES_NO_OPTION,
+						JOptionPane.QUESTION_MESSAGE,
+						null,     //do not use a custom Icon
+						options,  //the titles of buttons
+						options[0]);
+
+				if(answer == JOptionPane.YES_OPTION){
+					MainMenu.mainmenuInstance.UpdateNameLabels(listview, null, Field.getText(),listview.getFridge().returnName());
+					listview.getFridge().setNewName(Field.getText());
+					listview.revalidate();
+					listview.repaint();
+				}else if(answer == JOptionPane.NO_OPTION){
+					Object[] optionsalt2 = {"Please continue",
+					"Cancel"};
+					int result = JOptionPane.showOptionDialog(null,
+							"This action will permanently remove selected objects. Are you sure you want to proceed?",
+							"Confirmation PopUp",
+							JOptionPane.YES_NO_OPTION,
+							JOptionPane.WARNING_MESSAGE,
+							null,     //do not use a custom Icon
+							optionsalt2,  //the titles of buttons
+							optionsalt2[0]); //default button title
+					if(result == JOptionPane.YES_OPTION)
+						MainMenu.mainmenuInstance.DestroyObject(listview, null,listview.getFridge().returnName());
+				}
+			}else if(shoppingview instanceof ShoppingListView){
+				int answer = JOptionPane.showOptionDialog(null,
+						myPanel,
+						"Fridge Details",
+						JOptionPane.YES_NO_OPTION,
+						JOptionPane.QUESTION_MESSAGE,
+						null,     //do not use a custom Icon
+						optionsshop,  //the titles of buttons
+						optionsshop[0]);
+
+				if(answer == JOptionPane.YES_OPTION){
+					MainMenu.mainmenuInstance.UpdateNameLabels(null, shoppingview, Field.getText(),shoppingview.getShopList().getName());
+					shoppingview.getShopList().setName(Field.getText());
+					shoppingview.revalidate();
+					shoppingview.repaint();
+				}else if(answer == JOptionPane.NO_OPTION){
+					Object[] optionsalt = {"Please continue",
+					"Cancel"};
+					int result = JOptionPane.showOptionDialog(null,
+							"This action will permanently remove selected objects. Are you sure you want to proceed?",
+							"Confirmation PopUp",
+							JOptionPane.YES_NO_OPTION,
+							JOptionPane.WARNING_MESSAGE,
+							null,     //do not use a custom Icon
+							optionsalt,  //the titles of buttons
+							optionsalt[0]); //default button title
+					if(result == JOptionPane.YES_OPTION)
+						MainMenu.mainmenuInstance.DestroyObject(null, shoppingview,shoppingview.getShopList().getName());
+				}
+			}
+		}
+
+	}
+
+
+
